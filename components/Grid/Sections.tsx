068e58c16200d294b8c47f021882ff89d78e22cf
hector <hectorrrivera1010@gmail.com>
2024-03-10 20:06:21 -0600

    [add] sections component

--- components/Grid/Sections.tsx
+++ components/Grid/Sections.tsx
@@ -1,8 +1,28 @@
+import Link from "next/link";
 
 const Sections = () => {
-  return (
-    <div className="bg-zinc-500">Sections</div>
-  )
-}
 
-export default Sections
+  /*
+  ESTE ES SOLO UNA MAQUETACION CUANDO TENGA LOS ICONOS REALES USAR UN MAP
+  */
+
+  return <div className="grid grid-cols-3 gap-2 p-2 bg-zinc-500 rounded-md">
+    <Link href=''>
+      <div className="bg-zinc-700 h-20">
+        a
+      </div>
+    </Link>
+    <Link href=''>
+      <div className="bg-zinc-700 h-20">
+        a
+      </div>
+    </Link>
+    <Link href=''>
+      <div className="bg-zinc-700 h-20">
+        a
+      </div>
+    </Link>
+    </div>;
+};
+
+export default Sections;
