3f31c59964ad33fcd4ba8f7cef781bac656f7823
hector <hectorrrivera1010@gmail.com>
2024-03-11 00:27:18 -0600

    [add] interface for navigavtion links and sublinks

--- components/Grid/Title.tsx
+++ components/Grid/Title.tsx
@@ -1,7 +1,7 @@
 const Title = () => {
   return (
-    <div className="bg-zinc-800 text-white p-2 rounded-md md:pr-52">
-      <h1 className="text-2xl">Pascaline = Math + Open Source.</h1>
+    <div className="bg-zinc-800 text-white p-4 rounded-md md:pr-52">
+      <h1 className="text-2xl pb-2">Pascaline = Math + Open Source.</h1>
       <p className="text-zinc-400 text-pretty font-light">
         Navigate math, physics, and stats with ease. Essential tools for
         understanding and mastering these disciplines.
--- components/Navigation/NavigationLinks.tsx
+++ components/Navigation/NavigationLinks.tsx
@@ -1,28 +1,54 @@
 interface Link {
+  name: string;
+  submenu: boolean;
+  sublink: SubLinksContainer[];
+}
+
+interface SubLinksContainer {
+    head: string;
+    mySubLinks: SubLinks[]; 
+}
+
+interface SubLinks {
     name: string;
+    link: string;
 }
 
 const NavigationLinks = () => {
+  const links: Link[] = [
+    { name: "Math" , submenu: true, sublink: [
+        {
+            head: "Math11",
+            mySubLinks: [
+                {
+                    name: 'Matrix',
+                    link: "/Math/Matrix"
+                },
+                {
+                    name: 'Matrix Determinant',
+                    link: "/Math/Determinant"
+                },
+                {
+                    name: 'Triange Solver',
+                    link: "/Math/Triangle"
+                }
+            ]
+        }
+    ]},
 
-    const links: Link[] = [
-        {name: 'Math'},
-        {name: 'Physics'},
-        {name: 'Statistics'},
-    ]
+  ];
 
   return (
     <>
-        {
-            links.map((link) => (
-                <>
-                    <div className="md:mb-0 mb-6 cursor-pointer hover:text-gray-500">
-                        <h1 className="text-2xl md:text-sm">{link.name}</h1>
-                    </div>
-                </>
-            ))
-        }
+      {links.map((link) => (
+        <>
+          <div className="md:mb-0 mb-6 cursor-pointer hover:text-gray-500">
+            <h1 className="text-2xl md:text-sm">{link.name}</h1>
+          </div>
+        </>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default NavigationLinks
+export default NavigationLinks;
