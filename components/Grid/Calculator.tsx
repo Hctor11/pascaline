a473f037a85dbcfd290e7aad8c037a6e7d7daade
hector <hectorrrivera1010@gmail.com>
2024-03-18 20:16:59 -0600

    [add] calculator placeholder

--- components/Grid/Calculator.tsx
+++ components/Grid/Calculator.tsx
@@ -1,8 +1,8 @@
 
 const Calculator = () => {
   return (
-    <div className="bg-zinc-600 rounded-xl p-2 h-96">
-      <h1 className="text-white">Calculator</h1>
+    <div className="border rounded-xl flex justify-center items-center p-2 h-96 md:bg-[url('/images/CalculatorPlaceholder.svg')] bg-cover">
+      <h1 className="md:text-zinc-200">Under construction...</h1>
     </div>
   )
 }
