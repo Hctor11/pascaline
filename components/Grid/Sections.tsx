2a6110f02d867328cfd1d882c203f89df11e2317
hector <hectorrrivera1010@gmail.com>
2024-03-21 23:23:51 -0600

    [add] improved header

--- components/Grid/Sections.tsx
+++ components/Grid/Sections.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Sections = () => {
   /*
@@ -6,22 +7,46 @@ const Sections = () => {
   */
 
   return (
-    <div className="grid place-items-center md:w-[80%] lg:w-[40%] place-content-center grid-cols-3 gap-2 p-2  bg-zinc-500 rounded-xl">
-      <Link href="">
-        <div className="bg-zinc-700 h-28 md:h-20 lg:h-16 rounded-sm aspect-square">
-          a
-        </div>
-      </Link>
-      <Link href="">
-        <div className="bg-zinc-700 h-28 md:h-20 lg:h-16 rounded-sm aspect-square">
-          a
-        </div>
-      </Link>
-      <Link href="">
-        <div className="bg-zinc-700 h-28 md:h-20 lg:h-16 rounded-sm aspect-square">
-          a
-        </div>
-      </Link>
+    <div className="grid place-items-center md:w-[80%] lg:w-[40%] place-content-center grid-cols-3 gap-2 p-2 border rounded-xl">
+      <div className="flex flex-col items-center">
+        <Link href="">
+          <div className="bg-zinc-900 h-28 flex items-center justify-center md:h-20 lg:h-16 rounded-md aspect-square">
+            <Image
+              src="/categoryIcons/math.svg"
+              alt="math icon"
+              width={70}
+              height={70}
+            />
+          </div>
+        </Link>
+        <p>Math</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <Link href="">
+          <div className="bg-zinc-900 h-28 flex items-center justify-center md:h-20 lg:h-16 rounded-md aspect-square">
+            <Image
+              src="/categoryIcons/physics.svg"
+              alt="math icon"
+              width={60}
+              height={60}
+            />
+          </div>
+        </Link>
+        <p>Physics</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <Link href="">
+          <div className="bg-zinc-900 h-28 flex items-center justify-center md:h-20 lg:h-16 rounded-md aspect-square">
+            <Image
+              src="/categoryIcons/statistics.svg"
+              alt="math icon"
+              width={50}
+              height={50}
+            />
+          </div>
+        </Link>
+        <p>Statistics</p>
+      </div>
     </div>
   );
 };
