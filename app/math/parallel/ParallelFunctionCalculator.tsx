f2ef04e1019f645493b3c2ba844e5530f1800ee3
hector <hectorrrivera1010@gmail.com>
2024-03-22 23:01:51 -0600

    [add] omg I literally was missing one import hell na

--- app/math/parallel/ParallelFunctionCalculator.tsx
+++ app/math/parallel/ParallelFunctionCalculator.tsx
@@ -1,32 +1,44 @@
-import React from 'react'
-import PersonalizedButton from '@/components/Utilities/PersonalizedButton'
-import PersonalizedInput from '@/components/Utilities/PersonalizedInput'
-import Latex from 'react-latex-next'
+import React from "react";
+import PersonalizedButton from "@/components/Utilities/PersonalizedButton";
+import PersonalizedInput from "@/components/Utilities/PersonalizedInput";
+import Latex from "react-latex-next";
+import "katex/dist/katex.min.css";
 
 const ParallelFunctionCalculator = () => {
   return (
     <div>
       {" "}
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 gap-y-4 font-semibold">
         <span>
-          Linear function: {" "}
-          <div className="">
-            <Latex>
-              $y =\; $
-            </Latex>
-          <PersonalizedInput
-            placeholder="a"
-            name="x"
-            value={1}
-          />
-          <Latex>
-              $x$
-            </Latex>
+          input function:{" "}
+          <div className="flex items-center">
+            <div className="flex items-center gap-1">
+              <Latex>$y =\; $</Latex>
+              <PersonalizedInput placeholder="a" name="b" />
+              <Latex>$\;x$</Latex>
+              <select name="" className="ml-3 p-1 border rounded-md" id="">
+                <option value="">+</option>
+                <option value="">_</option>
+              </select>
+              <PersonalizedInput placeholder="b" name="b" />
+            </div>
+          </div>
+        </span>
+        <span>
+          input point:{" "}
+          <div className="flex items-center">
+            <div className="flex items-center gap-1">
+              <Latex>$P\, ($</Latex>
+              <PersonalizedInput placeholder="x" name="x" />
+              <Latex>$|$</Latex>
+              <PersonalizedInput placeholder="y" name="y" />
+              <Latex>$)$</Latex>
+            </div>
           </div>
         </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ParallelFunctionCalculator
+export default ParallelFunctionCalculator;
