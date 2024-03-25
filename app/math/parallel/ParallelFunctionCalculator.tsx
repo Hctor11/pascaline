101e07f06e8c5272fb2570c5b756b730e497f5b6
hector <hectorrrivera1010@gmail.com>
2024-03-24 20:36:33 -0600

    [add] useState values

--- app/math/parallel/ParallelFunctionCalculator.tsx
+++ app/math/parallel/ParallelFunctionCalculator.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import React from "react";
+import { useState } from "react";
 import PersonalizedButton from "@/components/Utilities/PersonalizedButton";
 import PersonalizedInput from "@/components/Utilities/PersonalizedInput";
 import Latex from "react-latex-next";
 import "katex/dist/katex.min.css";
 
 const ParallelFunctionCalculator = () => {
+
+  const [inputPoint, setInputPoint] = useState({x: undefined, y: undefined});
+  const inputSlope = useState(null)
+  const inputIntercept = useState(null)
+
   return (
     <div>
       {" "}
@@ -18,7 +26,7 @@ const ParallelFunctionCalculator = () => {
               <Latex>$\;x$</Latex>
               <select name="" className="ml-3 p-1 border rounded-md" id="">
                 <option value="">+</option>
-                <option value="">_</option>
+                <option value="">-</option>
               </select>
               <PersonalizedInput placeholder="b" name="b" />
             </div>
@@ -35,6 +43,7 @@ const ParallelFunctionCalculator = () => {
               <Latex>$)$</Latex>
             </div>
           </div>
+        <PersonalizedButton content="Calculate"/>
         </span>
       </div>
     </div>
