fd412e00caad6e5014a2d1193628832ae83574c7
hector <hectorrrivera1010@gmail.com>
2024-03-28 20:22:25 -0600

    [add] init calculator component

--- components/Grid/Calculator.tsx
+++ components/Grid/Calculator.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 
 const Calculator = () => {
+
+  const [firstInput, setFirstInput] = useState<null | number>(null)
+  const [secondInput, setSecondInput] = useState<null | number>(null)
+
+
   return (
-    <div className="border rounded-xl flex justify-center items-center p-2 h-96 md:bg-[url('/images/CalculatorPlaceholder.svg')] bg-cover">
-      <h2 className="md:text-zinc-200">Under construction...</h2>
+    <div className="border rounded-xl flex justify-center items-center p-2 h-96">
+      <input type="text" className="w-full border rounded-md h-12 text-xl px-2" placeholder=""/>
+      <div className="">
+        <button type="button" className=""></button>
+      </div>
     </div>
   )
 }
--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -5,7 +5,7 @@ import Categories from "./Categories";
 
 const Grid = () => {
   return (
-    <main className="box-border max-w-[1420px] gap-y-2 my-2 grid grid-cols-1 p-2 lg:w-[80%] lg:m-auto lg:mt-3">
+    <main className="box-border max-w-[1420px] gap-y-2 my-2 grid grid-cols-1 p-2 md:mx-3 lg:w-[80%] lg:m-auto lg:mt-3">
       <div className="m-auto overflow-clip gap-x-2 grid lg:grid-cols-[69%,30%] w-full">
         <div className="grid gap-y-2 grid-cols-1 lg:m-auto">
           <TitleAndSections />
