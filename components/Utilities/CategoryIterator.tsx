1dcf7127ade6a42b87ef1456d75eee9891e3f956
hector <hectorrrivera1010@gmail.com>
2024-04-05 19:26:05 -0600

    Update category iterator

--- components/Utilities/CategoryIterator.tsx
+++ components/Utilities/CategoryIterator.tsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 
 const CategoryIterator = ({ category }: props) => {
   return (
-    <div className="flex my-2 flex-col flex-wrap gap-2">
+    <div className="flex my-4 flex-col flex-wrap gap-2">
       {links.map((link) => (
         <>
           {link.name === category
             ? link.sublink.map((sublinks) => (
                 <>
-                  <h2 className="font-semibold">
+                  <h2 className="font-normal text-xl">
                     {sublinks.head}
                   </h2>
                   <div className="flex gap-2">
