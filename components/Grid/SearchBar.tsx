4ea6315fda14f430f25f705d0edf785d2a062030
hector <hectorrrivera1010@gmail.com>
2024-04-05 18:20:47 -0600

    Add search functionality

--- components/Grid/SearchBar.tsx
+++ components/Grid/SearchBar.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface card {
   image: string;
@@ -21,15 +24,35 @@ const cardsData: card[] = [
     description: "Given a point and a slope find the perpendicular funtion.",
     link: "/math/twopoints",
   },
+  {
+    image: "",
+    title: "Parallel equation finder",
+    description: "Given a point and a slope find the parallel funtion.",
+    link: "/math/parallel",
+  },
+  {
+    image: "",
+    title: "Unit converter",
+    description: "Convert any unit to other.",
+    link: "/phy/unit",
+  },
 ];
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredCards = cardsData.filter((card) =>
+    card.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="border p-2 rounded-xl hidden lg:flex lg:flex-col lg:justify-between">
       <div className="w-full flex justify-end items-center relative">
         <input
           placeholder="Search Calculator"
           className="border text-sm border-gray-400 rounded-lg p-2 w-full"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <Image
           width={18}
@@ -39,14 +62,26 @@ const SearchBar = () => {
           alt="Search Icon"
         />
       </div>
+      <div className="flex my-2 rounded-md flex-col gap-2 w-full h-full">
+        {searchTerm === "" ? null : filteredCards.map((card) => (
+          <Link href={card.link} key={card.title}>
+            <div className="p-2 max-h-20 bg-zinc-100 border hover:bg-zinc-200 rounded-md">
+              <h3 className="font-semibold text-sm">{card.title}</h3>
+              <p className="text-xs overflow-clip">{card.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
       <div className="flex flex-col gap-2">
-        <p className="cursor-default text-xs text-zinc-400">popular calculators</p>
-        {cardsData.map((card) => (
+        <p className="cursor-default text-xs text-zinc-400">
+          popular calculators
+        </p>
+        {cardsData.slice(0, 2).map((card) => (
           <>
             <Link href={card.link}>
-              <div className="p-2  bg-zinc-100 border hover:bg-zinc-200 rounded-md">
+              <div className="p-2 max-h-20 bg-zinc-100 border hover:bg-zinc-200 rounded-md">
                 <h3 className="font-semibold text-sm">{card.title}</h3>
-                <p className="text-xs">{card.description}</p>
+                <p className="text-xs overflow-clip">{card.description}</p>
               </div>
             </Link>
           </>
