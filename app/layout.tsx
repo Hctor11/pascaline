966a929f706d8e0f08760a5dac012d6e2b67eb7c
hector <hectorrrivera1010@gmail.com>
2024-04-08 19:54:31 -0600

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,12 @@ import "./globals.css";
 import Navigation from "@/components/Navigation/Navigation";
 import Footer from "@/components/Footer/Footer";
 import { GeistSans } from "geist/font/sans";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 
-const MathFont = localFont({src: './fonts/lmmath-regular.otf',variable: '--math-font'})
+const MathFont = localFont({
+  src: "./fonts/lmmath-regular.otf",
+  variable: "--math-font",
+});
 
 const geist = GeistSans;
 
@@ -22,6 +25,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>Pascaline - calculate everything</title>
+        <meta name="title" content="Pascaline - calculate everything" />
+        <meta name="description" content="All calculations in one place." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://pascaline.vercel.app/" />
+        <meta property="og:title" content="Pascaline - calculate everything" />
+        <meta
+          property="og:description"
+          content="All calculations in one place."
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://pascaline.vercel.app/" />
+        <meta
+          property="twitter:title"
+          content="Pascaline - calculate everything"
+        />
+        <meta
+          property="twitter:description"
+          content="All calculations in one place."
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+
         <link
           href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
           rel="stylesheet"
